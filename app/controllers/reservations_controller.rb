549c57fb5b4c16ae2fd4aed1e0b5b2dffa5eda90
Kelly <kellylovelu@gmail.com>
2017-09-08 14:38:20 -0400

    built reservations controller and views

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,7 +2,7 @@ class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:show, :edit, :update, :destroy]
 
   def index
-    @reservations = current_user.reservations.all
+    @reservation = current_user.reservations.all
   end
 
   def show
@@ -16,47 +16,35 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = current_user.reservations.new(reservation_params)
-
-    respond_to do |format|
-      if @reservation.save
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
-        format.json { render :show, status: :created, location: @reservation }
-      else
-        format.html { render :new }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
+    @pet = Pet.find(params[:id])
+    @reservation = @pet.reservations.build(reservation_params)
+    @reservation.user = current_user
+  
+    if @reservation.save
+      flash[:notice] = "Reservation completed."
+      redirect_to @reservation
+    else
+      flash[:danger] = @reservation.errors
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @reservation.update(reservation_params)
-        format.html { redirect_to @reservation, notice: 'Reservation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @reservation }
-      else
-        format.html { render :edit }
-        format.json { render json: @reservation.errors, status: :unprocessable_entity }
-      end
-    end
+
   end
 
   def destroy
     @reservation.destroy
-    respond_to do |format|
-      format.html { redirect_to reservations_url, notice: 'Reservation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def reservation_params
-      params.require(:reservation).permit(:month, :day, :year, :borrower_id, :pet_id, :avatar)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def reservation_params
+    params.require(:reservation).permit(:month, :day, :year, :user_id, :pet_id)
+  end
 end
